7629f63e70fa5189eed900e7c0f63eacc1fb3d51
nachitoderacing <166016638+nachitoderacing@users.noreply.github.com>
2024-08-15 08:16:55 -0300

    Add files via upload
    let numeros = []
    for(let i = 0; i < 5; i++) {
        numeros.push=prompt("Ingrese un número:");
        numeros.push(numeros);
    }
    
    console.log(numeros);
    
        for (let index = 0; index < numeros.lenght; index++) 
        if (index = 20)
        index = index*2

---
+++ index.js
@@ -0,0 +1,11 @@
+let numeros = []
+for(let i = 0; i < 5; i++) {
+    numeros.push=prompt("Ingrese un número:");
+    numeros.push(numeros);
+}
+
+console.log(numeros);
+
+    for (let index = 0; index < numeros.lenght; index++) 
+    if (index = 20)
+    index = index*2
